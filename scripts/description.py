3900fc37450412307e2424b266e8cbb81349fbff
SeanJ.Morris <seanj.morris@gmail.com>
2020-11-15 13:00:03 -0500

    Deleted data_cleaning.py and functions.py scripts and created description.py script to complete this assignment.

---
+++ scripts/description.py
@@ -0,0 +1,9 @@
+#Capstone Project In Jypyter Notebook
+_by Sean Morris_
+
+### This code is for the Data Science Captsone Project for the IBM Coursera Data Science Certificate
+
+import pandas as pd
+import numpy as np
+
+print ("Hello Capstone Project Course!")
